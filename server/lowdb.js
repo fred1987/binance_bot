92424cd760ea90e8ec1024e2ae2463decaa4694c
Alfred Ivanov <hi@ifred.ru>
2021-08-10 13:37:31 +0300

    fix

--- server/lowdb.js
+++ server/lowdb.js
@@ -1,7 +1,7 @@
 import {join} from 'path';
 import {Low, JSONFile} from 'lowdb';
 
-const root = join.bind(this, process.cwd(), './server/');
+const root = join.bind(this, process.cwd(), './');
 const adapter = new JSONFile(root('db.json'));
 const db = new Low(adapter);
 
